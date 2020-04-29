8fb6fe199015ceba645dd7fd9abf3584001d5d03
Bevis Pei <ppf@jiumao.org>
2020-04-29 13:56:03 +0800

    test

---
+++ src/main/java/org/jit/util/ProcessUtils.java
@@ -0,0 +1,17 @@
+package org.jit.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author ppf
+ */
+public class ProcessUtils {
+
+    public static void main(String[] args) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        Process start = processBuilder.start();
+        OutputStream outputStream = start.getOutputStream();
+        start.destroy();
+    }
+}
