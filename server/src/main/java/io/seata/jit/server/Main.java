2fee71b8d9d164d8db33a8549c5d9db5f92dc3b2
peipengfei <wb-ppf652528@antfin.com>
2020-04-30 17:24:17 +0800

    xxx

--- server/src/main/java/io/seata/jit/server/Main.java
+++ server/src/main/java/io/seata/jit/server/Main.java
@@ -82,7 +82,7 @@ public class Main {
 
     public static TestCase getTccTestCase() {
         TestCase tcc = new TestCase("Tcc", "spring dubbo");
-        tcc.addService("java -cp .:tcc/target/lib/*:tcc/target/seata-jit-tcc.jar io.seata.jit.starter.TccProviderStarter", 60);
+        tcc.addService("java -cp .:tcc/target/lib/*:tcc/target/seata-jit-tcc.jar io.seata.jit.starter.TccProviderStarter", 120);
         tcc.addService("java -cp .:tcc/target/lib/*:tcc/target/seata-jit-tcc.jar io.seata.jit.starter.TccConsumerStarter", 60);
 
         return tcc;
