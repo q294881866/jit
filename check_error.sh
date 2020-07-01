0c6cb09bb8b2e2e394c368b59629968570223b21
peipengfei <wb-ppf652528@antfin.com>
2020-07-01 16:11:18 +0800

    log

--- check_error.sh
+++ check_error.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-if [ $(find ./  -maxdepth 1 -name "*error.log" | xargs grep "ERROR" |wc -l) -gt 0 ]; then exit 1; fi
+if [ $(find ~/logs -name "*.log" | xargs grep "ERROR" |wc -l) -gt 0 ]; then exit 1; fi
 
